e584fcfda9efca5412e00f999a5a22284ed64eac
Baskar <baymax1298@gmail.com>
2018-01-19 19:21:57 +0530

    added forgot password

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,6 +1,7 @@
 const User = require("../models/usermodel");
 const jwt = require("jwt-simple");
 const config = require("../config");
+const mailer = require("nodemailer");
 
 function tokenForUser(user){
   const timestamp = new Date().getTime();
@@ -14,6 +15,52 @@ function tokenForUser(user){
   )
 }
 
+exports.createforgotpasswordlink = function(req,res,next){
+  let email = req.body.email;
+  User.findOne({email:email},function(err,existingUser){
+    if(err){return next(err)}
+
+    if(existingUser){
+
+
+      var trans = mailer.createTransport(
+      {service :"gmail",
+      auth:{
+      user: "baymax1298@gmail.com",
+      pass: "smily2012"
+
+     }});
+
+      var link = "http://localhost:65000/forgot-password/"+tokenForUser(existingUser);
+      let img = "https://hdwallsource.com/img/2014/8/colorful-background-pictures-17219-17775-hd-wallpapers.jpg"
+      var mailOptions = {
+      from: '<baymax1298@yahoo.com>',
+      to: email,
+      subject: "Password Reset",
+      text: "Password Reset",
+      html:"<br><center><div style="+
+      "background:url(https://hdwallsource.com/img/2014/8/colorful-background-pictures-17219-17775-hd-wallpapers.jpg);padding:15px"+
+      "><h1>Hi "+ existingUser.username +"</h1>"+
+           "<h3>You've successfully changed your password.</br>"+
+           "<br>Here's your new password  "+"<a href="+ link +">link</a> to reset your password"+"</br>"+
+           "<br>Thanks for using !</br>"+
+           "<br>The  Team</br></h3></div></center>"
+     };
+
+      trans.sendMail(mailOptions, function(error, info){
+      if (error) {
+          console.log(error);
+      }else{
+      console.log('Message sent:', info.messageId, info.response);
+      res.send({success:"mailsentsuccesfully"})
+           }
+      });
+
+    }else{
+      res.send({error:"username does not exists"})
+    }
+  })
+}
 
 exports.signin = function(req,res,next){
 
@@ -23,8 +70,9 @@ exports.signin = function(req,res,next){
 exports.signup = function(req,res,next){
   const email = req.body.email;
   const password = req.body.password;
+  const username = req.body.username;
 
-  if(!email||!password){
+  if(!email||!password||!username){
     res.status(422).send({error:"checkout all field"})
   }
   // check wheather email id exists in db
@@ -37,7 +85,8 @@ exports.signup = function(req,res,next){
   // create user
   const user = new User({
     email:email,
-    password:password
+    password:password,
+    username:username
   })
   // save user
   user.save(function(err){
